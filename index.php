e34f91eadd51ce54cfdb21dcc76cc856733fcd9a
Ihor Radchenko <radchenko1995@gmail.com>
2017-10-29 09:29:59 +0200

    Final

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ihor
+ * Date: 29.10.2017
+ * Time: 9:27
+ */
+
+include_once $_SERVER['DOCUMENT_ROOT'] . '/includes/config.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/includes/function.php';
+
+redirect_to(DOMEN . 'public/');
